7399da9a010b0badf9e3d4cf1e5e11a3cbb69f62
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2022-09-05 21:30:19 -0400

    args to inp, main comented - pre

--- components/preprocess_data.py
+++ components/preprocess_data.py
@@ -10,19 +10,19 @@ from component_utils.general import create_artifact_folder
 logger = logging.getLogger()
 
 
-def go(args):
-    artifact_path = create_artifact_folder(__file__)
+def go(input):
+    artifact_path = 'artifacts'
 
     nlp = spacy.load("en_core_web_sm")
     sws = nlp.Defaults.stop_words
-    exs = args.exclude.split(",")
+    exs = input['exclude'].split(",")
     if len(exs) > 0:
         sws.update(exs)
     
     logger.info("Reading data from input file...")
 
     data = (
-        pd.read_csv(args.input_path, sep = "\t")
+        pd.read_csv(input['input_path'], sep = "\t")
         .drop_duplicates()
     )
     data["Date"] = pd.to_datetime(data["Date"])
@@ -40,11 +40,11 @@ def go(args):
 
     data.to_csv(artifact_path / "metoo_data.csv", sep = "\t")
 
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("mode", type = str, help = "Run mode (local or remote)")
-    parser.add_argument("input_path", type = str, help = "Path to input data (.zip)") 
-    parser.add_argument("exclude", type = str, default = "", help = "Additional (comma-separated) stopwords to exclude")       
-    args = parser.parse_args()
+# if __name__ == "__main__":
+#     parser = ArgumentParser()
+#     parser.add_argument("mode", type = str, help = "Run mode (local or remote)")
+#     parser.add_argument("input_path", type = str, help = "Path to input data (.zip)") 
+#     parser.add_argument("exclude", type = str, default = "", help = "Additional (comma-separated) stopwords to exclude")       
+#     args = parser.parse_args()
 
-    go(args)
+#     go(args)
