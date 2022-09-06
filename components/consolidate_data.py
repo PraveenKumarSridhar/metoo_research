00a6c03e1cd0eaf5a18101aa6b0170d01d799ea8
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2022-09-05 20:57:34 -0400

    comented out main

--- components/consolidate_data.py
+++ components/consolidate_data.py
@@ -45,10 +45,10 @@ def consolidate_data(input):
             )
 
 
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("mode", type = str, help = "Run mode (local or remote)")
-    parser.add_argument("input_path", type = str, help = "Path to input data (.zip)") 
-    parser.add_argument("--samp_size", type = int, required = False, default = None, help = "Number of files to sample data from")     
-    args = parser.parse_args()
-    go(args)
+# if __name__ == "__main__":
+#     parser = ArgumentParser()
+#     parser.add_argument("mode", type = str, help = "Run mode (local or remote)")
+#     parser.add_argument("input_path", type = str, help = "Path to input data (.zip)") 
+#     parser.add_argument("--samp_size", type = int, required = False, default = None, help = "Number of files to sample data from")     
+#     args = parser.parse_args()
+#     go(args)
