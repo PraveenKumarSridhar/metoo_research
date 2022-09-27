ed2ab6505c92e847edd797e58cac6fdef5d88e17
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2022-09-27 12:41:10 -0400

    Upated infer news to create a new news inference model
    TODO: Update config yaml, postprocess

--- components/infer_news.py
+++ components/infer_news.py
@@ -7,46 +7,71 @@ from argparse import ArgumentParser
 
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+from imblearn.under_sampling import RandomUnderSampler 
+from imblearn.over_sampling import SMOTE
+
+from imblearn.pipeline import Pipeline 
+
+import pickle
+import logging
+
 import numpy as np
 import pandas as pd
 from pathlib import Path
+logger = logging.getLogger()
 
 
+def news_iden_imp_heu(author,news_id_list):
+    auth_heuristics = 'news' in str(author).lower() or author in news_id_list
+    return 1 if auth_heuristics else 0
 
-def go(args):
+def go(input):
     artifact_path = Path('components/artifacts/')
 
-    data = pd.read_csv(args.input_path, sep = "\t")
+    data = pd.read_csv(input['input_path'], sep = "\t")
     data["Full Text"] = data["Full Text"].fillna("")
+
+    news_ids = pd.read_csv(r'../data/news_outlets-accounts.csv')
+    news_id_list = news_ids['Token'].tolist()
+
+    author_corpus = data.groupby("Author")["Full Text"].apply(" ".join).reset_index()
+    author_corpus ['news_label'] = author_corpus['Author'].apply(lambda x: news_iden_imp_heu(x,news_id_list))
 
     author_corpus = data.groupby("Author")["Full Text"].apply(" ".join)
     
     news_inf = pd.Series(author_corpus.index.str.contains("news").astype(int), index = author_corpus.index)
 
-    vec = TfidfVectorizer(max_features = args.vocab_size)
-    
     X_train, X_test, y_train, y_test = train_test_split(
-        author_corpus, 
-        news_inf,
-        random_state = args.random_state,
-        train_size = args.train_size
-    )
+            author_corpus['Full Text'], 
+            author_corpus['news_label'],
+            random_state = input['random_state'],
+            train_size = input['train_size']            
+        )
 
-    X_train_vec = vec.fit_transform(X_train)
-    X_test_vec = vec.transform(X_test)
+    if input['sampling_tech'] == 'SMOTE':
+        nb_pipe3  = Pipeline([('vect', CountVectorizer()),
+                        ('tfidf',   TfidfTransformer()),
+                        ('sampler', SMOTE('minority',random_state=input['random_state'])),
+                        ('model',   MultinomialNB())])
+    else:
+        nb_pipe3  = Pipeline([('vect', CountVectorizer()),
+                     ('tfidf',   TfidfTransformer()),
+                     ('sampler', RandomUnderSampler('majority',random_state=input['random_state'])),
+                     ('model',   MultinomialNB())])
 
-    model = MultinomialNB()
-    model.fit(X_train_vec, y_train)
 
-    train_preds = pd.Series(model.predict(X_train_vec), index = X_train.index)
-    test_preds = pd.Series(model.predict(X_test_vec), index = X_test.index)
-    preds = pd.concat([train_preds, test_preds])
+    model = nb_pipe3.fit(X_train, y_train)
 
-    news_inf = news_inf.replace({0: np.nan}).combine_first(preds)
-    
+    test_preds =  model.predict(X_test)
+
     cnf = confusion_matrix(y_test, test_preds)
-    print(cnf)
+    logger.info("CONFUSION MATRIX OF NEWS PREDICTION")
+    logger.info(cnf)
 
-    news_inf.to_csv(artifact_path / "news.csv")
+    # save the model to disk
+    filename = input['output']
+    pickle.dump(model, open(filename, 'wb'))
