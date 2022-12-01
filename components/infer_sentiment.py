bcf9721708d8498471ee3ae24c1232462065fe6c
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2022-12-01 02:12:54 -0500

    updated infer sentiment to use spacy

--- components/infer_sentiment.py
+++ components/infer_sentiment.py
@@ -6,14 +6,22 @@ import spacy
 from pathlib import Path
 from transformers import pipeline
 import torch
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
 
 
 logger = logging.getLogger()
+nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe('spacytextblob')
 
+def use_spacy_get_sentiment(text):
+    spacy_txt = nlp(text)
+    sentiment = spacy_txt._.blob.polarity
+    return 'pos' if sentiment > 0 else 'neg'  
 
 def go(input):
     artifact_path = Path('components/artifacts/')
-    sentiment_pipeline = pipeline("sentiment-analysis")
+    # sentiment_pipeline = pipeline("sentiment-analysis")
     
     logger.info("Reading data from input file...")
 
@@ -23,6 +31,8 @@ def go(input):
         .fillna('')
     )
 
-    data['sentiment'] = data['Full Text'].apply(lambda x: sentiment_pipeline(x)[0]['label'])
+    # data['sentiment'] = data['Full Text'].apply(lambda x: sentiment_pipeline(x)[0]['label'])
+    data['sentiment'] = data['Full Text'].apply(lambda x: use_spacy_get_sentiment(x))
+
 
     data.to_csv(artifact_path / input['output'], sep = "\t")
