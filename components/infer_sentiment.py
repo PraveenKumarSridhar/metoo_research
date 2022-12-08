1673fa71a77cae0025dd8db61be39df40d315f52
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2022-12-08 13:01:38 -0500

    changed print to log

--- components/infer_sentiment.py
+++ components/infer_sentiment.py
@@ -22,7 +22,7 @@ logger = logging.getLogger()
 def go(input):
     artifact_path = Path('components/artifacts/')
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    print(f'Sentiment pipeline will run on {device}')
+    logger.info(f'Sentiment pipeline will run on {device}')
     sentiment_pipeline = pipeline("sentiment-analysis", device = 0)
     
     logger.info("Reading data from input file...")
