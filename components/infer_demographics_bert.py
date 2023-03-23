02756a85dee1a080e18781bd287e00dc6f204fcd
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2023-03-23 14:32:17 -0400

    Added error handling

--- components/infer_demographics_bert.py
+++ components/infer_demographics_bert.py
@@ -32,10 +32,14 @@ def read_tweet_text_from_timeline_custom(user_id, timeline_dir):
 
 
 def get_demographics(user_id, user_data_dir, demographer_list):
-    user_with_multiple_texts = read_tweet_text_from_timeline_custom(user_id = user_id, timeline_dir = user_data_dir)
-    # logger.info(user_with_multiple_texts)
-    result = process_multiple_tweet_texts(user_with_multiple_texts, demographer_list)
-    return result['eth_selfreport_bert']['value']
+    try:
+        user_with_multiple_texts = read_tweet_text_from_timeline_custom(user_id = user_id, timeline_dir = user_data_dir)
+        # logger.info(user_with_multiple_texts)
+        result = process_multiple_tweet_texts(user_with_multiple_texts, demographer_list)
+        return result['eth_selfreport_bert']['value']
+    except Exception as e:
+        logger.error(e)
+        return 'Error'
 
 def go(input):
     logger.info('Starting set up for demographer')
