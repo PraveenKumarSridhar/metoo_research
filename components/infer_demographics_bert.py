6e177577c1ab35b547ccd308137183b59cea48df
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2023-03-23 01:25:44 -0400

    commented log statements

--- components/infer_demographics_bert.py
+++ components/infer_demographics_bert.py
@@ -33,7 +33,7 @@ def read_tweet_text_from_timeline_custom(user_id, timeline_dir):
 
 def get_demographics(user_id, user_data_dir, demographer_list):
     user_with_multiple_texts = read_tweet_text_from_timeline_custom(user_id = user_id, timeline_dir = user_data_dir)
-    logger.info(user_with_multiple_texts)
+    # logger.info(user_with_multiple_texts)
     result = process_multiple_tweet_texts(user_with_multiple_texts, demographer_list)
     return result['eth_selfreport_bert']['value']
 
